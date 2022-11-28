1ef6b21890fb18def653391f9b87394883c38c26
Harsh P <harshpmpm07@gmail.com>
2022-11-28 11:13:51 +0530

    second

--- src/Component/Mode.jsx
+++ src/Component/Mode.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 function Mode() {
   return (
+    <div className="mode">
+        <span>Light</span>
     <div className='toggle-switch'>
         <label >
                 <input type="checkbox" />
                 <span className='slider'></span>
         </label>
+    </div>
+    <span>Dark</span>
     </div>
   )
 }
