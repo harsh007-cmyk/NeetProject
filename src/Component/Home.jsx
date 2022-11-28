c33ea8bff9f03f4ad1e02c64c5212c1fb141232c
Harsh P <harshpmpm07@gmail.com>
2022-11-28 16:33:16 +0530

    3rd

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,31 +1,23 @@
 import React from 'react'
 var lastscrolled=0;
+
 function Home() {
-//         function handleScroll(){
-//     const im1=document.querySelector('.Home_mid_image');
-//     const im2=document.querySelector('.Home_sec_image');
-//     const stl=window.getComputedStyle(im1);
-//     const mrgtp=stl.marginTop.replace( /[^\d\.]*/g, '');
-//     const scrolled=window.scrollY;
-//     console.log(mrgtp);   
-//         if(scrolled>0&&scrolled<833){
-//     if(scrolled>lastscrolled){
-//         console.log('up');
-//         im1.style.marginTop=`${Number(mrgtp)-5}px`;
-//     }else if(scrolled<lastscrolled){
-//         console.log('donw');
-//         im1.style.marginTop=`${Number(mrgtp)+5}px`;
-//     }
-    
-//     if(scrolled<=0){
-//         lastscrolled=0;
-//     }else{
-//         lastscrolled=scrolled;
-//     }
-// }
-//     }
-    // window.addEventListener('scroll',handleScroll);
-    // console.log('llllllllllllllllllll');
+    function handleScroll(){
+        const im1=document.querySelector('.Home-mid-image');
+        const im2=document.querySelector('.Home_sec_image');
+        const scrolled=window.scrollY;
+        // if(scrolled*0.2>80)
+        console.log(window.getComputedStyle(im2).marginTop);
+        im1.style.marginTop=`${90-scrolled*0.2}px`;
+        console.log(scrolled);
+        console.log(im1);
+        
+    }
+     
+
+    window.addEventListener('scroll',handleScroll);
+
+//     console.log('llllllllllllllllllll');
   return (
     <div className='Home_section'>
         <div className="Home_text_section">
@@ -42,11 +34,11 @@ function Home() {
         </div>
         
         </div>
-        <div className="Home_mid_image">
+        <div className="Home-mid-image">
             <img src={require('../Images/scrollImage.png')} alt="" />
         </div>
         <div className='Home_sec_image'>
-            <img src={require('../Images/screens02.png')} alt="" />
+        <img src={require('../Images/screens02.png')} alt="" />
         </div>
     </div>
   )
